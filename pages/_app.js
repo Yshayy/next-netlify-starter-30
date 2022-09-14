adb849955cac36ef8055c90feb6b1e3b8819d977
Yshayy <yshayy@gmail.com>
2022-09-14 13:54:50 +0300

    3 files modified

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
-import '@styles/globals.css'
+import '@styles/globals.css';
+import { LivecycleSdk } from '@livecycle/sdk';
+import { useEffect } from 'react';
 
 function Application({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    LivecycleSdk.init();
+  });
+  return <Component {...pageProps} />;
 }
 
-export default Application
+export default Application;
